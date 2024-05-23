54c2309803a322c0a258ebb567ae6e10c6c8504a
sallen1851 <sallen1851@gmail.com>
2024-05-22 21:38:57 -0400

    insert HTML into card div

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
+let users = [];
+
 // FETCH DATA
 async function getUsers() {
-  const response = await fetch('https://randomuser.me/api/?inc=picture,location,phone,gender,name,email,dob');
+  const response = await fetch('https://randomuser.me/api/?inc=picture,location,phone,gender,name,email,dob&results=12');
   const data = await response.json();
   const users = data.results;
   displayUsers(users);
@@ -8,23 +10,32 @@ async function getUsers() {
   return users;
 }  
 
-function displayUsers(users) {
-    const usersContainer = document.querySelector('.search-container')
-    const usersHTML = users
-    .map(
-        (user) => `
-        <div class="card">
-            <div class="card-img-container">
-                <img class="card-img" src="${user.picture.thumbnail}" alt="profile picture">
-            </div>
-            <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
-                <p class="card-text">${user.email}</p>
-                <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
-            </div>`
-    )
-    .join('');
-    usersContainer.insertAdjacentHTML('beforeend', usersHTML);
-}
+// DISPLAY USERS
 
-getUsers();
+function displayUsers(users) {
+    const usersContainer = document.getElementById('gallery');
+    usersContainer.innerHTML = ''; 
+
+    users.forEach((user, index) => { // loop through each user
+      const div = document.createElement('div'); // create a new div
+      div.classList.add('card'); // add the card class to the div
+      // give the card div a custom data attribute matching its index in the users array
+      div.setAttribute('data-index', index);
+      // generate HTML
+      const userHTML = `
+        <div class="card-img-container">
+            <img class="card-img" src="${user.picture.medium}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 class="card-name cap">${user.name.first} ${user.name.last}</h3>
+            <p class="card-text">${user.email}</p>
+            <p class="card-text cap">${user.location.city}, ${user.location.country}</p>
+        </div>
+      `;
+  
+      div.insertAdjacentHTML('beforeend', userHTML); // insert the HTML into the card div
+        usersContainer.appendChild(div); // append the card div to the gallery div
+    });
+  }
+  
+  getUsers();
