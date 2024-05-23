003975555993ba1b98af86ed3c1040aa228fd0ec
sallen1851 <sallen1851@gmail.com>
2024-05-22 20:44:19 -0400

    fetched data

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-<test></test>
+// FETCH DATA
+async function getUsers() {
+  const response = await fetch('https://randomuser.me/api/?inc=picture,location,phone,gender,name,nat');
+  const data = await response.json();
+    console.log(data);
+  return data;
+}
+
+getUsers();
