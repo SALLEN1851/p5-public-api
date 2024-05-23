dd0a69c129f706f429d61fe02524d91a80e8bc6e
sallen1851 <sallen1851@gmail.com>
2024-05-22 21:10:26 -0400

    displayUsers function and generate HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
 // FETCH DATA
 async function getUsers() {
-  const response = await fetch('https://randomuser.me/api/?inc=picture,location,phone,gender,name,nat');
+  const response = await fetch('https://randomuser.me/api/?inc=picture,location,phone,gender,name,email,dob');
   const data = await response.json();
-    console.log(data);
-  return data;
+  const users = data.results;
+  displayUsers(users);
+    console.log(users);
+  return users;
+}  
+
+function displayUsers(users) {
+    const usersContainer = document.querySelector('.search-container')
+    const usersHTML = users
+    .map(
+        (user) => `
+        <div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${user.picture.thumbnail}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+                <p class="card-text">${user.email}</p>
+                <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+            </div>`
+    )
+    .join('');
+    usersContainer.insertAdjacentHTML('beforeend', usersHTML);
 }
 
 getUsers();
